3ecd5c16297b9ff2a7c2e8981f1015744993e65c
Kevin Alexis Martínez Herrera <alexismtz_99@icloud.com>
2022-05-06 12:26:39 -0500

    Whatever

--- WebAPI/App_Start/RouteConfig.cs
+++ WebAPI/App_Start/RouteConfig.cs
@@ -13,11 +13,14 @@ namespace WebAPI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //Especifica la ruta y estructura, que se debe seguir por defecto
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
+
+       //Aquí podemos especificar que fue por ruta
     }
 }
