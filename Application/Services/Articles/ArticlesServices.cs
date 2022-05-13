d2f8dd9a6f4442e38bc567f97c7763a935e6d176
Kevin Alexis Martínez Herrera <alexismtz_99@icloud.com>
2022-05-13 15:51:08 -0500

    Módulo Articulo
    Se implementó el módulo para artículos con sus métodos CRUD

---
+++ Application/Services/Articles/ArticlesServices.cs
@@ -0,0 +1,49 @@
+﻿using Application.Interfaces;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Articles
+{
+   public class ArticlesServices
+    {
+
+        private readonly IMemoryStorage<Article> _memoryStorage;
+
+        public ArticlesServices(IMemoryStorage<Article> memoryStorage)
+        {
+            _memoryStorage = memoryStorage;
+        }
+
+
+        public string Create(Article article)
+        {
+           return _memoryStorage.Create(article);
+        }
+
+        public string Edit(Article article)
+        {
+            return _memoryStorage.Edit(article);
+        }
+
+        public string Delete(string id)
+        {
+            return _memoryStorage.Delete(id);
+        }
+
+        public List<Article> GetAll()
+        {
+            return _memoryStorage.GetAll();
+        }
+
+        public Article GetById(string id)
+        {
+            return _memoryStorage.GetById(id);
+        }
+
+
+    }
+}
---
+++ MVC/Areas/Admin/Controllers/ArticlesController.cs
@@ -0,0 +1,83 @@
+﻿using Application.Interfaces;
+using Application.Services.Articles;
+using Domain.Models;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Areas.Admin.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        private readonly IMemoryStorage<Article> _memoryStorage;
+
+        public ArticlesController()
+        {
+            _memoryStorage = new MemoryStorage<Article>();
+        }
+
+        // GET: Admin/Articles
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var articleServices = new ArticlesServices(_memoryStorage);
+
+            var articles = articleServices.GetAll();
+
+            return View(articles.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Article article)
+        {
+            var articleServices = new ArticlesServices(_memoryStorage);
+
+            articleServices.Create(article);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            var articleServices = new ArticlesServices(_memoryStorage);
+
+            var article = articleServices.GetById(id);
+         
+            return View(article);
+        }
+
+
+        public ActionResult Edit(Article article)
+        {
+            var articleServices = new ArticlesServices(_memoryStorage);
+
+            articleServices.Edit(article);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public ActionResult Delete(string id)
+        {
+            var articleServices = new ArticlesServices(_memoryStorage);
+
+            articleServices.Delete(id);
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+    }
+}
